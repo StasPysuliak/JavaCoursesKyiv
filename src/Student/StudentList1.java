007cd41485edd72cb74a6d5cdf63afda2c0372d9
Stanislav <statpusuliak@gmail.com>
2019-08-10 15:49:36 +0300

    Home Work 2 v2.5

---
+++ src/Student/StudentList1.java
@@ -0,0 +1,71 @@
+package Student;
+
+import java.util.ArrayList;
+
+public class StudentList {
+    private  ArrayList<Student> list = new ArrayList<>();
+    private int num;
+
+    public void add(Student student) {
+        list.add(student);
+        num = list.size();
+    }
+
+    public int getNum() {
+        System.out.println(num);
+        return num;
+    }
+
+    public int findName(String name) {
+        for(int some = 0; some < num; some++) {
+            if(list.get(some).getName().equalsIgnoreCase(name)) {
+                getFullInfo(some);
+                return some;
+            }
+        }
+        return 0;
+    }
+
+    public int findSurname(String surName) {
+        for(int some = 0; some < num; some++) {
+            if(list.get(some).getSurname().equalsIgnoreCase(surName)) {
+                getFullInfo(some);
+                return some;
+            }
+        }
+        return 0;
+    }
+
+    public int findDate(String date) {
+        for(int some = 0; some < num; some++) {
+            if(list.get(some).getSurname().equalsIgnoreCase(date)) {
+                getFullInfo(some);
+                return some;
+            }
+        }
+        return 0;
+    }
+
+    public void remove(String name) {
+        for(int some = 0; some < num; some++) {
+            if (list.get(some).getName().equalsIgnoreCase(name)) {
+                list.remove(some);
+                num--;
+            }
+        }
+    }
+
+    public int getFullInfo(int some) {
+        String info = String.format("%s %s %s", list.get(some).getName(),
+                list.get(some).getSurname(), list.get(some).getBirth());
+        System.out.println(info);
+        return 0;
+    }
+
+    public int showFullInfo() {
+        for (int some = 0; some < num; some++)
+                getFullInfo(some);
+        return 0;
+    }
+
+}
