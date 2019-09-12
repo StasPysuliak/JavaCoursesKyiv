48000fde29f49fa9f2979741caec9355313e64fb
Stanislav <statpusuliak@gmail.com>
2019-09-12 19:13:40 +0300

    Home Work 3

--- src/figures/Figures.java
+++ src/figures/Figures.java
@@ -1,6 +1,7 @@
 package figures;
 
-public abstract class Figures implements Area{
+public abstract class Figures{
     double width = 3.2;
     double height = 4.1;
+    abstract double getArea();
 }
