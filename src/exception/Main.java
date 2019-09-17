5a49e2f6bb757f795d3f9989c159854753cc81ff
Stanislav <statpusuliak@gmail.com>
2019-09-17 14:52:31 +0300

    Final version

--- src/exception/Main.java
+++ src/exception/Main.java
@@ -1,9 +1,13 @@
 package exception;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String url = "para1=value1&param2=value2&param3=value3";
+        Scanner in = new Scanner(System.in);
+        String url = in.nextLine();
+        //String url = "para1=value1&param2=value2&param3=value3";
         URLtest urLtest = new URLtest();
         try {
             urLtest.ifURLisCorrect(url);
