c7e6d0cd1848f85884ad7011c64129cd804fb320
Stanislav <statpusuliak@gmail.com>
2020-03-10 21:05:38 +0200

    Home Work 5

---
+++ src/findFiles/Main.java
@@ -0,0 +1,40 @@
+package findFiles;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    static class MyFileFilter implements FilenameFilter {
+        private String ext;
+        public MyFileFilter(String ext) {
+            this.ext = ext;
+        }
+        public boolean accept(File dir, String name) {
+            return name.endsWith(ext);
+        }
+    }
+
+    private static void findFiles(String srcPath, String[] ext, ArrayList<String> list) throws IOException {
+        File dir = new File(srcPath);
+        for (String e : ext) {
+            File[] files = dir.listFiles(new MyFileFilter(e));
+            for (int i = 0; i < files.length; i++) {
+                list.add(srcPath + files[i].getName());
+                //list.add (files[i].getCanonicalPath());
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        String[] ext = {".txt",".pdf",".iso"};
+        try {
+            findFiles("c:\\folder", ext, list);
+        } catch (IOException ex) {}
+        for (String s : list)
+            System.out.println(s);
+    }
+
+}
