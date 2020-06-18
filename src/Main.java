75c79a94fe0ff9d14df4e689ea67c42f4cba63bf
Stanislav <statpusuliak@gmail.com>
2020-06-18 20:37:32 +0300

    Home Work 9

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String tex;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        bf.readLine().split(" ");
+
+        System.out.println(bf.readLine().split(" ")[0]);
+        System.out.println(bf.readLine().split(" ")[1]);
+        System.out.println(bf.readLine().split(" ")[2]);
     }
 }
--- src/peopleSerialization/Main.java
+++ src/peopleSerialization/Main.java
@@ -9,18 +9,22 @@ public class Main {
     public static void main(String[] args) {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         List<People> list = new ArrayList<>();
 
-        for (int i = 0; i < 2; i++) {
+
+        for (int i = 0; i < 10; i++) {
             try {
-                list.add(new People(bf.readLine(), bf.readLine(), Integer.parseInt(bf.readLine())));
+                System.out.println("Please enter: Name SurName and age");
+                System.out.println("Example: Ryan Reynolds 43");
+                String peopleData = bf.readLine();
+                list.add(new People(peopleData.split(" ")[0], peopleData.split(" ")[1],
+                                    Integer.parseInt(peopleData.split(" ")[2])));
             }
             catch (Exception e) {
                 ;
             }
         }
-        System.out.println(list.toString());
+        System.out.println(list);
 
     }
 }
